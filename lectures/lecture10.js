ad5ee6c405c01fc6b5c4bdb2ed1921e87218975a
MD_Hasan Patwary <MD_Hasan>
2019-06-03 12:25:37 +0600

    Javascript Foreach Function Implementation

---
+++ lectures/lecture10.js
@@ -0,0 +1,27 @@
+//Javascript Foreach Function Implementation  /  Class-10
+
+//Example-1
+
+var arr = [1, 2, 3, 4, 5, 6];
+
+// arr.forEach(function(element, index, arr) {
+//     console.log('Element is = ' + element + ' Index is = ' + index + ' arr is = ' + arr);  
+// });
+
+
+
+//Example-1
+
+// function loop(arr) {
+//     for(var i = 0; i < arr.length; i++) {
+//         console.log(arr[i]);
+//     }
+// }
+// loop(arr);
+
+function loop(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+loop(arr);
