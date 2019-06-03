f5d7a5b5715984db1a467bd1ae1f94da8dfae44c
MD_Hasan Patwary <MD_Hasan>
2019-06-03 12:28:45 +0600

    Javascript Foreach Function Implementation

--- lectures/lecture10.js
+++ lectures/lecture10.js
@@ -2,7 +2,7 @@
 
 //Example-1
 
-var arr = [1, 2, 3, 4, 5, 6];
+// var arr = [1, 2, 3, 4, 5, 6];
 
 // arr.forEach(function(element, index, arr) {
 //     console.log('Element is = ' + element + ' Index is = ' + index + ' arr is = ' + arr);  
@@ -10,8 +10,8 @@ var arr = [1, 2, 3, 4, 5, 6];
 
 
 
-//Example-1
-
+//Example-2
+var arr = [1, 2, 3, 4, 5, 6];
 // function loop(arr) {
 //     for(var i = 0; i < arr.length; i++) {
 //         console.log(arr[i]);
@@ -19,9 +19,12 @@ var arr = [1, 2, 3, 4, 5, 6];
 // }
 // loop(arr);
 
-function loop(arr) {
+function loop(arr, callback) {
     for (var i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
+        callback(arr[i], i, arr);
     }
 }
-loop(arr);
+loop(arr, function (element, index, arr) {
+    console.log('Element is ' + element + ' Index is = ' + index + ' array is = ' + arr);
+
+});
